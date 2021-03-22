1aea6ca54f6c0c2da1e1f9f351f039f634054c04
=amartinn <martoy123691@gmail.com>
2021-03-22 15:25:06 +0200

    feat:repository

---
+++ server/Movies.Server/Data/Common/IDeletableEntityRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace Movies.Server.Data.Common
+{
+    using Models.Base;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public interface IDeletableEntityRepository<TEntity> : IRepository<TEntity>
+        where TEntity : class, IDeletableEntity
+    {
+        IQueryable<TEntity> AllWithDeleted();
+
+        IQueryable<TEntity> AllAsNoTrackingWithDeleted();
+
+        void HardDelete(TEntity entity);
+
+        void Undelete(TEntity entity);
+
+        void Delete(TEntity entity);
+    }
+}
