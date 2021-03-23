5a4dd217dd27c1602dc78a72ae7371c8036e2568
=amartinn <martoy123691@gmail.com>
2021-03-23 03:49:20 +0200

    rating routes implementation

---
+++ server/Movies.Server/Features/Ratings/IRatingService.cs
@@ -0,0 +1,16 @@
+﻿namespace Movies.Server.Features.Ratings
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Features.Ratings.Models;
+    using Infrastructure.Services;
+
+    public interface IRatingService
+    {
+        Task<Result> AddRatingToMovie(int value, int movieId, string userId);
+
+        Task<Result> UpdateRating(int value, int movieId, string userId);
+
+        IEnumerable<RatingListingServiceModel> GetRatingsByUserId(string userId);
+    }
+}
